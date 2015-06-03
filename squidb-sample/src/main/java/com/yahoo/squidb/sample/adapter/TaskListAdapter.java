1509fde3b252df0d2d613085fdff700cec5ad215
Sam Bosley <sboz88@gmail.com>
2015-06-03 10:06:54 -0700

    Basic adapter implementation

--- squidb-sample/src/main/java/com/yahoo/squidb/sample/adapter/TaskListAdapter.java
+++ squidb-sample/src/main/java/com/yahoo/squidb/sample/adapter/TaskListAdapter.java
@@ -8,8 +8,11 @@ package com.yahoo.squidb.sample.adapter;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.yahoo.squidb.sample.R;
 import com.yahoo.squidb.sample.models.Task;
+import com.yahoo.squidb.sample.utils.TaskUtils;
 import com.yahoo.squidb.utility.SquidCursorAdapter;
 
 public class TaskListAdapter extends SquidCursorAdapter<Task> {
@@ -20,6 +23,27 @@ public class TaskListAdapter extends SquidCursorAdapter<Task> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        if (convertView == null) {
+            convertView = getLayoutInflater().inflate(R.layout.task_row, parent);
+            convertView.setTag(new TaskRowViewHolder(convertView));
+        }
+
+        TaskRowViewHolder viewHolder = (TaskRowViewHolder) convertView.getTag();
+
+        Task item = getItem(position);
+        viewHolder.taskTitle.setText(item.getTitle());
+        viewHolder.taskTags.setText(item.get(TaskUtils.TAGS_CONCAT));
+        return convertView;
+    }
+
+    private static class TaskRowViewHolder {
+
+        TextView taskTitle;
+        TextView taskTags;
+
+        public TaskRowViewHolder(View row) {
+            taskTitle = (TextView) row.findViewById(R.id.task_title);
+            taskTags = (TextView) row.findViewById(R.id.task_tags);
+        }
     }
 }
