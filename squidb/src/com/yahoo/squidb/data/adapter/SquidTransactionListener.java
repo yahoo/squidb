7edb27d0c1464037d51b62b3bf400752eabe06ba
Sam Bosley <sboz88@gmail.com>
2015-08-10 14:55:18 -0700

    Add documentation to all of the new wrapper classes

--- squidb/src/com/yahoo/squidb/data/adapter/SquidTransactionListener.java
+++ squidb/src/com/yahoo/squidb/data/adapter/SquidTransactionListener.java
@@ -5,12 +5,28 @@
  */
 package com.yahoo.squidb.data.adapter;
 
+import android.database.sqlite.SQLiteTransactionListener;
+
+/**
+ * A re-declaration of {@link android.database.sqlite.SQLiteTransactionListener} so that all wrapper/adapter classes
+ * for different versions of {@link android.database.sqlite.SQLiteDatabase} can provide a unified interface for
+ * transaction methods
+ */
 public interface SquidTransactionListener {
 
+    /**
+     * @see SQLiteTransactionListener#onBegin()
+     */
     void onBegin();
 
+    /**
+     * @see SQLiteTransactionListener#onCommit()
+     */
     void onCommit();
 
+    /**
+     * @see SQLiteTransactionListener#onRollback()
+     */
     void onRollback();
 
 }
