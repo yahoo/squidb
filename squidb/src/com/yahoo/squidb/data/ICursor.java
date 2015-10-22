a8d3d285b4734cad26cdd10bc6e7ac16ff44fad1
Sam Bosley <sboz88@gmail.com>
2015-10-22 15:24:20 -0700

    Update the license in ICursor to reflect that it's a fork of android.database.Cursor

--- squidb/src/com/yahoo/squidb/data/ICursor.java
+++ squidb/src/com/yahoo/squidb/data/ICursor.java
@@ -1,7 +1,21 @@
 /*
- * Copyright 2015, Yahoo Inc.
- * Copyrights licensed under the Apache 2.0 License.
- * See the accompanying LICENSE file for terms.
+ * Copyright (C) 2006 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * This class is a fork of android.database.Cursor free of any Android-specific dependencies. It is used to provide
+ * a common interface that various SQLite adapters can conform to when returning query results
  */
 package com.yahoo.squidb.data;
 
