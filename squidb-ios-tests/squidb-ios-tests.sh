42dd0fab3840f7b90a46149417123dbd01125791
Sam Bosley <sboz88@gmail.com>
2015-10-22 13:24:02 -0700

    Clean up and comment ios test script

--- squidb-ios-tests/squidb-ios-tests.sh
+++ squidb-ios-tests/squidb-ios-tests.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 if [ ! -f "${J2OBJC_HOME}/j2objc" ]; then echo "J2OBJC_HOME not correctly defined, currently set to '${J2OBJC_HOME}'"; exit 1; fi;
+
+# clean up build directories
 rm *.o
 if [ -d bin ]
 then
@@ -44,21 +46,22 @@ SOURCEPATH="${GEN_SRC}:${SQUIDB_SRC}:${SQUIDB_ANNOTATIONS_SRC}:${SQUIDB_IOS_SRC}
 # invoke annotation processing, output to gen folder
 javac -AsquidbOptions=iosModels -classpath "${J2OBJC_HOME}/lib/j2objc_junit.jar:./squidb-annotations-2.0.0.jar:./squidb-processor-2.0.0.jar:./apt-utils-1.0.0.jar" -s gen/ -proc:only -sourcepath "${SOURCEPATH}" ${SQUIDB_TESTS_TEST_SRC}/**/*.java
 
+# invoke j2objc to translate java sources
 ${J2OBJC_HOME}/j2objc -classpath "${J2OBJC_HOME}/lib/j2objc_junit.jar" -d intermediate --no-package-directories -use-arc -sourcepath "${SOURCEPATH}" ${SQUIDB_SRC}/**/*.java \
     ${SQUIDB_IOS_SRC}/**/*.java ${SQUIDB_TESTS_TEST_SRC}/*.java ${GEN_SRC}/**/*.java \
     ${SQUIDB_IOS_TESTS_SRC}/**/*.java ${SQUIDB_TESTS_DATA_SRC}/*.java ${SQUIDB_TESTS_SQL_SRC}/*.java  ${SQUIDB_TESTS_UTILITY_SRC}/*.java
 
-#"${J2OBJC_HOME}/j2objc" -d ${DERIVED_FILES_DIR} -classpath "${J2OBJC_HOME}/lib/j2objc_junit.jar:${PROJECT_DIR}/squidb-ios-tests/squidb-annotations-2.0.0.jar" -sourcepath "${PROJECT_DIR}/../squidb-ios/src:${PROJECT_DIR}/../squidb/src:${PROJECT_DIR}/../squidb-tests/src:${PROJECT_DIR}/squidb-ios-tests/java" --no-package-directories -use-arc -g ${INPUT_FILE_PATH};
-
+# compile translated Obj-C sources
 for f in ../squidb-ios/native/**/*.m **/*.m
 do
     echo "Compiling $f"
     ${J2OBJC_HOME}/j2objcc -fobjc-arc -Iintermediate -I../squidb-ios/native -c $f # output to bin folder
 done
 
+# build test executable
 mv *.o bin/
-
 ${J2OBJC_HOME}/j2objcc -L ${J2OBJC_HOME}/lib/macosx -l jre_emul -l junit -l sqlite3 -ObjC -o invoke_ios_tests bin/*.o # link with libraries
 
+# run tests
 ./invoke_ios_tests com.yahoo.squidb.test.SquidbTestRunner
 rm invoke_ios_tests
