a33adb621a93c59254b065bd324a4ebae30caaf8
Sam Bosley <sboz88@gmail.com>
2015-10-22 08:19:01 -0700

    Make compilation of .m files work without errors

--- squidb-ios-tests/squidb-ios-tests.sh
+++ squidb-ios-tests/squidb-ios-tests.sh
@@ -30,11 +30,13 @@ SOURCEPATH="${GEN_SRC}:${SQUIDB_SRC}:${SQUIDB_ANNOTATIONS_SRC}:${SQUIDB_IOS_SRC}
 javac -AsquidbOptions=iosModels -classpath "${J2OBJC_HOME}/lib/j2objc_junit.jar:./squidb-annotations-2.0.0.jar:./squidb-processor-2.0.0.jar:./apt-utils-1.0.0.jar" -s gen/ -proc:only -sourcepath "${SOURCEPATH}" ${SQUIDB_TESTS_TEST_SRC}/**/*.java
 
 if [ ! -f "${J2OBJC_HOME}/j2objc" ]; then echo "J2OBJC_HOME not correctly defined, currently set to '${J2OBJC_HOME}'"; exit 1; fi;
-${J2OBJC_HOME}/j2objc -classpath "${J2OBJC_HOME}/lib/j2objc_junit.jar" -d intermediate -sourcepath "${SOURCEPATH}" ${SQUIDB_SRC}/**/*.java \
+${J2OBJC_HOME}/j2objc -classpath "${J2OBJC_HOME}/lib/j2objc_junit.jar" -d intermediate --no-package-directories -use-arc -sourcepath "${SOURCEPATH}" ${SQUIDB_SRC}/**/*.java \
     ${SQUIDB_IOS_SRC}/**/*.java ${SQUIDB_TESTS_TEST_SRC}/*.java ${GEN_SRC}/**/*.java \
     ${SQUIDB_IOS_TESTS_SRC}/**/*.java ${SQUIDB_TESTS_DATA_SRC}/*.java ${SQUIDB_TESTS_SQL_SRC}/*.java  ${SQUIDB_TESTS_UTILITY_SRC}/*.java
 
-${J2OBJC_HOME}/j2objcc -Iintermediate -I../squidb-ios/native -c ../squidb-ios/native/**/*.m **/*.m # output to bin folder
+#"${J2OBJC_HOME}/j2objc" -d ${DERIVED_FILES_DIR} -classpath "${J2OBJC_HOME}/lib/j2objc_junit.jar:${PROJECT_DIR}/squidb-ios-tests/squidb-annotations-2.0.0.jar" -sourcepath "${PROJECT_DIR}/../squidb-ios/src:${PROJECT_DIR}/../squidb/src:${PROJECT_DIR}/../squidb-tests/src:${PROJECT_DIR}/squidb-ios-tests/java" --no-package-directories -use-arc -g ${INPUT_FILE_PATH};
+
+${J2OBJC_HOME}/j2objcc -fobjc-arc -Iintermediate -I../squidb-ios/native -c ../squidb-ios/native/**/*.m **/*.m # output to bin folder
 
 mv *.o bin/
 
