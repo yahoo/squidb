453740160c6e8e8bced8a49362a88b1110913dc4
Sam Bosley <sboz88@gmail.com>
2015-10-31 21:24:52 -0700

    Add the @Constants annotation for annotating static inner classes in model specs that hold constants
    Most constants can safely be copied directly to the generated model. However, constants that are defined using table schema fields
    (e.g. from Properties, Table, View, like public static final Order DEFAULT_ORDER = Model.COLUMN.asc()) may run into class loading
    order issues where the fields needed to initialize the constant have not yet themselves been initialized at class loading time.
    (This is discussed in more detail in issue 110, https://github.com/yahoo/squidb/pull/110).
    
    This commit allows constants to be declared in a static inner class of the model spec, effectively deferring initialization
    of copied constants until after the full schema has been initialized. The old behavior of directly copying constants has been
    left intact so those who don't need this control over class loading order can use things as they always have.

--- squidb-tests/src/com/yahoo/squidb/data/ModelDependentSpecTest.java
+++ squidb-tests/src/com/yahoo/squidb/data/ModelDependentSpecTest.java
@@ -7,6 +7,7 @@ package com.yahoo.squidb.data;
 
 import com.yahoo.squidb.test.DatabaseTestCase;
 import com.yahoo.squidb.test.ModelDependent;
+import com.yahoo.squidb.test.TestViewModel;
 
 public class ModelDependentSpecTest extends DatabaseTestCase {
 
@@ -20,4 +21,13 @@ public class ModelDependentSpecTest extends DatabaseTestCase {
         }
     }
 
+    public void testModelDependentConstantFromInnerClassIsAccessible() {
+        try {
+            TestViewModel.DEFAULT_ORDER.getClass();
+        } catch (ExceptionInInitializerError e) {
+            fail("The generated model must define constants that depend on other constants after " +
+                    "their dependencies. Fields marked `static final` are initialised in the order " +
+                    "they appear in the source.");
+        }
+    }
 }
