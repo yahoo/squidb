032073bf8252998151382ca25fb4eb5a8912e432
Sam Bosley <sboz88@gmail.com>
2016-01-23 20:42:51 -0800

    Add some missing file headers

--- squidb-processor/src/com/yahoo/squidb/processor/plugins/defaults/properties/EnumFieldPlugin.java
+++ squidb-processor/src/com/yahoo/squidb/processor/plugins/defaults/properties/EnumFieldPlugin.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2015, Yahoo Inc.
+ * Copyrights licensed under the Apache 2.0 License.
+ * See the accompanying LICENSE file for terms.
+ */
 package com.yahoo.squidb.processor.plugins.defaults.properties;
 
 import com.yahoo.aptutils.model.DeclaredTypeName;
