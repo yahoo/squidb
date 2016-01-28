5cc8ed182ea5446e53e530ded733ec3f9c3687e1
Sam Bosley <sboz88@gmail.com>
2016-01-28 10:11:42 -0800

    Don't need to explicitly run the iOS twice anymore for CI -- the script already does it

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,5 +4,4 @@ then
     adb shell am instrument -w -e squidb_binding $SQUIDB_BINDING com.yahoo.squidb.test/com.yahoo.squidb.android.SquidInstrumentationTestRunner
 else
     ./scripts/squidb-ios-tests.sh
-    ./scripts/squidb-ios-tests.sh --custom-sqlite
 fi
