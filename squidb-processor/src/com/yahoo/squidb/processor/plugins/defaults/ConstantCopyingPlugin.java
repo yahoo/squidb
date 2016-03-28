431bc2c594e2413799469651c9c3fb95c9d8bbaa
Sam Bosley <sboz88@gmail.com>
2016-03-28 14:53:20 -0700

    Fix handling of static final fields in model specs

--- squidb-processor/src/com/yahoo/squidb/processor/plugins/defaults/ConstantCopyingPlugin.java
+++ squidb-processor/src/com/yahoo/squidb/processor/plugins/defaults/ConstantCopyingPlugin.java
@@ -54,8 +54,7 @@ public class ConstantCopyingPlugin extends Plugin {
         if (field.getAnnotation(Deprecated.class) != null) {
             return false;
         }
-        Set<Modifier> modifiers = field.getModifiers();
-        if (modifiers.containsAll(TypeConstants.PUBLIC_STATIC_FINAL)) {
+        if (TypeConstants.isVisibleConstant(field)) {
             constantList.add(field);
             return true;
         }
@@ -119,7 +118,7 @@ public class ConstantCopyingPlugin extends Plugin {
     }
 
     private void writeConstantField(JavaFileWriter writer, DeclaredTypeName containingClassName,
-            VariableElement constant) throws IOException{
+            VariableElement constant) throws IOException {
         writer.writeFieldDeclaration(
                 utils.getTypeNameFromTypeMirror(constant.asType()),
                 constant.getSimpleName().toString(),
