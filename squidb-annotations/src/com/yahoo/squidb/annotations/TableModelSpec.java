6eedf5c072dafd1d5244f1e463657fdda4fa4d8f
Sam Bosley <sboz88@gmail.com>
2016-07-06 13:40:35 -0700

    Update a doc in TableModelSpec annotation

--- squidb-annotations/src/com/yahoo/squidb/annotations/TableModelSpec.java
+++ squidb-annotations/src/com/yahoo/squidb/annotations/TableModelSpec.java
@@ -39,8 +39,8 @@ public @interface TableModelSpec {
 
     /**
      * Optional flag to specify that the table model should not generate an explicit INTEGER PRIMARY KEY as an alias
-     * to the table's rowid. This option should be enabled if some non-integer column is to be declared a primary key
-     * or if a multi-column primary key is specified in {@link #tableConstraint()}.
+     * to the table's rowid. This option should be enabled if a multi-column primary key is specified in
+     * {@link #tableConstraint()}, or if the user does not wish to declare an explicit alias to the rowid column.
      */
     boolean noRowIdAlias() default false;
 
