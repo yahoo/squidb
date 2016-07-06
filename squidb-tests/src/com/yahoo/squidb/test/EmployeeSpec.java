2606106eef714f02559f8739bc722c331f3d4254
Sam Bosley <sboz88@gmail.com>
2016-07-06 10:38:11 -0700

    Explicitly declare _id primary keys as rowid aliases in test suite model specs to remove warnings

--- squidb-tests/src/com/yahoo/squidb/test/EmployeeSpec.java
+++ squidb-tests/src/com/yahoo/squidb/test/EmployeeSpec.java
@@ -6,10 +6,15 @@
 package com.yahoo.squidb.test;
 
 import com.yahoo.squidb.annotations.ColumnSpec;
+import com.yahoo.squidb.annotations.PrimaryKey;
 import com.yahoo.squidb.annotations.TableModelSpec;
 
 @TableModelSpec(className = "Employee", tableName = "employees")
 public class EmployeeSpec {
+
+    @PrimaryKey
+    @ColumnSpec(name = "_id")
+    long id;
 
     @ColumnSpec(constraints = "NOT NULL")
     String name;
--- squidb-tests/src/com/yahoo/squidb/test/TriggerTesterSpec.java
+++ squidb-tests/src/com/yahoo/squidb/test/TriggerTesterSpec.java
@@ -6,10 +6,15 @@
 package com.yahoo.squidb.test;
 
 import com.yahoo.squidb.annotations.ColumnSpec;
+import com.yahoo.squidb.annotations.PrimaryKey;
 import com.yahoo.squidb.annotations.TableModelSpec;
 
 @TableModelSpec(className = "TriggerTester", tableName = "trigger_testers")
 public class TriggerTesterSpec {
+
+    @PrimaryKey
+    @ColumnSpec(name = "_id")
+    long id;
 
     @ColumnSpec(defaultValue = "0")
     int value1;
