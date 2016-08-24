20114c99c094d364fa9df60742f634ae4019f948
Sam Bosley <sboz88@gmail.com>
2016-08-24 14:36:17 -0700

    Try to reduce our travis workload by combining the two different binding variants for android builds into a single job

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ -z "$CI_IOS_TESTS" ]
 then
-    adb shell am instrument -w -e squidb_binding $SQUIDB_BINDING com.yahoo.squidb.test/com.yahoo.squidb.android.SquidInstrumentationTestRunner
+    ./scripts/squidb-android-tests.sh
 else
     ./scripts/squidb-ios-tests.sh
 fi
---
+++ scripts/squidb-android-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+runAndroidTestWithBinding () {
+    echo "Running tests on platform $ANDROID_TARGET with binding $1"
+    adb shell am instrument -w -e squidb_binding $1 com.yahoo.squidb.test/com.yahoo.squidb.android.SquidInstrumentationTestRunner
+    testResult=$?
+    if [ ! $testResult -eq 0 ]
+    then
+        echo "Test suite failed on $ANDROID_TARGET with binding $1"
+        exit $testResult
+    fi
+}
+
+runAndroidTestWithBinding android
+if [ ! "$ANDROID_TARGET" = "android-15" ]
+then
+    runAndroidTestWithBinding sqlite
+fi
