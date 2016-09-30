3557c93d63384019e3fee47318c3521ccf97041e
Sam Bosley <sboz88@gmail.com>
2016-09-30 11:21:41 -0700

    Add additional documentation to ArgumentResolver

--- squidb/src/com/yahoo/squidb/sql/ArgumentResolver.java
+++ squidb/src/com/yahoo/squidb/sql/ArgumentResolver.java
@@ -10,13 +10,26 @@ package com.yahoo.squidb.sql;
  * lower-level primitives that are bound to the SQLite query at execution time. For example, an implementation of
  * this interface might unwrap the contents of an AtomicReference, or convert an AtomicBoolean into an integer.
  * <p>
- * The default implementation of this interface that SquiDB uses can be found in {@link DefaultArgumentResolver}
+ * The default implementation of this interface that SquiDB uses can be found in {@link DefaultArgumentResolver}.
+ * Users who wish to handle custom types should generally prefer to subclass DefaultArgumentResolver to handle said
+ * types, which avoids changing SquiDB's default handling of other argument types. If you really want to override
+ * SquiDB's default argument handling, you can implement this interface directly.
  */
 public interface ArgumentResolver {
 
     /**
-     * Resolve the given argument into a primitive suitable for binding to a low-level SQLite statement. If the
-     * argument resolver does not map the given argument into a number, string, boolean, blob, etc., it will eventually
+     * Resolve the given argument into a primitive suitable for binding to a low-level SQLite statement. Suitable
+     * primitives are:
+     * <ul>
+     * <li>Floating-point numbers (float, double), which will be bound as double</li>
+     * <li>Integer numbers (int, long, or any non-floating point subclass of Number),
+     * which will be bound as long</li>
+     * <li>Boolean, which will be bound as 1 for true or 0 for false</li>
+     * <li>byte[], which will be bound as a blob</li>
+     * <li>String</li>
+     * <li>null</li>
+     * </ul>
+     * If the argument resolver does not map the given argument into one of these primitive types, it will eventually
      * be bound to the statement as a string using toString().
      */
     Object resolveArgument(Object arg);
