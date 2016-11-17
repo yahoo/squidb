fd4acdd6c53f0e8f05ed2378330077d08868fddb
Sam Bosley <sboz88@gmail.com>
2016-11-16 16:08:20 -0800

    Implement the error logging in ErrorLoggingProcessor

--- squidb-processor/src/com/yahoo/squidb/processor/ErrorLoggingProcessor.java
+++ squidb-processor/src/com/yahoo/squidb/processor/ErrorLoggingProcessor.java
@@ -5,23 +5,28 @@
  */
 package com.yahoo.squidb.processor;
 
+import com.yahoo.aptutils.utils.AptUtils;
 import com.yahoo.squidb.annotations.ModelGenErrors;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeMirror;
+import javax.tools.Diagnostic;
 
 public class ErrorLoggingProcessor extends AbstractProcessor {
 
     private Set<String> supportedAnnotationTypes = new HashSet<>();
-    private Messager messager;
+    private AptUtils utils;
 
     public ErrorLoggingProcessor() {
         supportedAnnotationTypes.add(ModelGenErrors.class.getName());
@@ -40,7 +45,7 @@ public class ErrorLoggingProcessor extends AbstractProcessor {
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
-        this.messager = processingEnv.getMessager();
+        this.utils = new AptUtils(processingEnv);
     }
 
     @Override
@@ -48,15 +53,46 @@ public class ErrorLoggingProcessor extends AbstractProcessor {
         for (TypeElement annotationType : annotations) {
             if (supportedAnnotationTypes.contains(annotationType.getQualifiedName().toString())) {
                 for (Element element : env.getElementsAnnotatedWith(annotationType)) {
-                    ModelGenErrors errorsToLog = element.getAnnotation(ModelGenErrors.class);
-                    logErrors(errorsToLog);
+                    logErrors(element);
                 }
             }
         }
         return true;
     }
 
-    private void logErrors(ModelGenErrors errors) {
-        // TODO: Implement error logging
+    @SuppressWarnings("unchecked")
+    private void logErrors(Element element) {
+        AnnotationValue errorsArrayValue = utils.getAnnotationValue(element, ModelGenErrors.class, "value");
+        List<? extends AnnotationValue> errorsList = (List<? extends AnnotationValue>) errorsArrayValue.getValue();
+        for (AnnotationValue error : errorsList) {
+            logSingleError(error);
+        }
+    }
+
+    private void logSingleError(AnnotationValue singleErrorAnnotation) {
+        AnnotationMirror singleErrorMirror = (AnnotationMirror) singleErrorAnnotation.getValue();
+
+        TypeMirror errorClass = utils.getTypeMirrorsFromAnnotationValue(
+                utils.getAnnotationValueFromMirror(singleErrorMirror, "specClass")).get(0);
+        String errorMessage = utils.getValuesFromAnnotationValue(
+                utils.getAnnotationValueFromMirror(singleErrorMirror, "message"), String.class).get(0);
+        List<String> errorElementValues = utils.getValuesFromAnnotationValue(
+                utils.getAnnotationValueFromMirror(singleErrorMirror, "element"), String.class);
+        String errorElementName = AptUtils.isEmpty(errorElementValues) ? null : errorElementValues.get(0);
+
+        Element errorElement = findErrorElement(errorClass, errorElementName);
+        utils.getMessager().printMessage(Diagnostic.Kind.ERROR, errorMessage,
+                errorElement);
+    }
+
+    private Element findErrorElement(TypeMirror errorClass, String elementName) {
+        Element errorClassElement = utils.getTypes().asElement(errorClass);
+        List<? extends Element> enclosedElements = errorClassElement.getEnclosedElements();
+        for (Element e : enclosedElements) {
+            if (e.getSimpleName().toString().equals(elementName)) {
+                return e;
+            }
+        }
+        return errorClassElement;
     }
 }
