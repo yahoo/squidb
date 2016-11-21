9a51c4bede4afe947612e87e53a708694a905835
Sam Bosley <sboz88@gmail.com>
2016-11-21 13:03:40 -0800

    Move comments to public javadocs

--- squidb-processor/src/com/yahoo/squidb/processor/data/ErrorInfo.java
+++ squidb-processor/src/com/yahoo/squidb/processor/data/ErrorInfo.java
@@ -13,9 +13,20 @@ import com.yahoo.aptutils.model.DeclaredTypeName;
  */
 public final class ErrorInfo {
 
-    public final DeclaredTypeName errorClass; // The class on which to log the error
-    public final String element; // The specific element on which to log the error (or null/empty to log on the class)
-    public final String message; // The error message to log
+    /**
+     * The class on which to log the error
+     */
+    public final DeclaredTypeName errorClass;
+
+    /**
+     * The specific element on which to log the error (or null/empty to log on the class)
+     */
+    public final String element;
+
+    /**
+     * The error message to log
+     */
+    public final String message;
 
     public ErrorInfo(DeclaredTypeName errorClass, String element, String message) {
         this.errorClass = errorClass;
