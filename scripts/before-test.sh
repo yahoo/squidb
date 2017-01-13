78e00a47d4378644368663e4bbcea7b2b0f7e94f
Sam Bosley <sboz88@gmail.com>
2017-01-13 10:47:48 -0800

    Make travis run the squidb-processor tests

--- scripts/before-test.sh
+++ scripts/before-test.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-if [ -z "$CI_IOS_TESTS" ]
+if [ ! -z "$CI_IOS_TESTS" ]
 then
+    wget https://github.com/google/j2objc/releases/download/$J2OBJC_VERSION/j2objc-$J2OBJC_VERSION.zip -O /tmp/j2objc-$J2OBJC_VERSION.zip
+    unzip -oq /tmp/j2objc-$J2OBJC_VERSION.zip -d /tmp
+elif [ ! -z "$CI_CODE_GENERATOR_TESTS" ]
+then
+    echo "Running code generator tests"
+else
     sudo apt-get -qq update
     sudo apt-get install python3
 
@@ -9,7 +15,4 @@ then
     android-wait-for-emulator
     adb shell input keyevent 82 &
     ./gradlew squidb-tests:installDebug squidb-tests:installDebugAndroidTest
-else
-    wget https://github.com/google/j2objc/releases/download/$J2OBJC_VERSION/j2objc-$J2OBJC_VERSION.zip -O /tmp/j2objc-$J2OBJC_VERSION.zip
-    unzip -oq /tmp/j2objc-$J2OBJC_VERSION.zip -d /tmp
 fi
