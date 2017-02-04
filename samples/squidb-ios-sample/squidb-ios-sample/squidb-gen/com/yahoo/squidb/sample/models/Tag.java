91da4e09100a7882514f89887d5cf61491605142
Sam Bosley <sboz88@gmail.com>
2017-02-03 16:26:24 -0800

    Update iOS sample project to work with latest

--- samples/squidb-ios-sample/squidb-ios-sample/squidb-gen/com/yahoo/squidb/sample/models/Tag.java
+++ samples/squidb-ios-sample/squidb-ios-sample/squidb-gen/com/yahoo/squidb/sample/models/Tag.java
@@ -1,125 +1,122 @@
+// Generated code -- do not modify!
 package com.yahoo.squidb.sample.models;
 
 import com.yahoo.squidb.data.SquidCursor;
 import com.yahoo.squidb.data.TableModel;
 import com.yahoo.squidb.data.ValuesStorage;
 import com.yahoo.squidb.sql.Property;
-import com.yahoo.squidb.sql.Property.LongProperty;
-import com.yahoo.squidb.sql.Property.StringProperty;
 import com.yahoo.squidb.sql.Table;
 import com.yahoo.squidb.sql.TableModelName;
-import java.util.Map;
+import java.lang.Long;
+import java.lang.Override;
+import java.lang.String;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * This class was generated from the model spec at {@link com.yahoo.squidb.sample.models.TagSpec}
  */
-// Generated code -- do not modify!
 public class Tag extends TableModel {
+	private static final List<Property<?>> PROPERTIES_INTERNAL = new ArrayList<>(3);
 
-    // --- allocate properties array
-    public static final Property<?>[] PROPERTIES = new Property<?>[3];
+	public static final List<Property<?>> PROPERTIES = Collections.unmodifiableList(PROPERTIES_INTERNAL);
 
-    // --- table declaration
-    public static final Table TABLE = new Table(Tag.class, PROPERTIES, "tags", null, "FOREIGN KEY(taskId) references tasks(_id) ON DELETE CASCADE");
-    public static final TableModelName TABLE_MODEL_NAME = new TableModelName(Tag.class, TABLE.getName());
+	public static final Table TABLE = new Table(Tag.class, PROPERTIES, "tags", null, "FOREIGN KEY(taskId) references tasks(_id) ON DELETE CASCADE");
 
-    // --- property declarations
-    public static final LongProperty ID = new LongProperty(TABLE_MODEL_NAME, "_id", "PRIMARY KEY AUTOINCREMENT");
-    static {
-        TABLE.setRowIdProperty(ID);
-    };
+	public static final TableModelName TABLE_MODEL_NAME = new TableModelName(Tag.class, TABLE.getName());
 
-    @Override
-    public LongProperty getRowIdProperty() {
-        return ID;
-    }
+	public static final Property.LongProperty ID = new Property.LongProperty(TABLE_MODEL_NAME, "_id", "PRIMARY KEY AUTOINCREMENT");
 
+	public static final Property.StringProperty TAG = new Property.StringProperty(TABLE_MODEL_NAME, "tag", "NOT NULL");
 
-    public static final StringProperty TAG = new StringProperty(TABLE_MODEL_NAME, "tag", "NOT NULL");
+	public static final Property.LongProperty TASK_ID = new Property.LongProperty(TABLE_MODEL_NAME, "taskId", "NOT NULL");
 
-    public static final LongProperty TASK_ID = new LongProperty(TABLE_MODEL_NAME, "taskId", "NOT NULL");
+	protected static final ValuesStorage defaultValues = new Tag().newValuesStorage();
 
-    static {
-        PROPERTIES[0] = ID;
-        PROPERTIES[1] = TAG;
-        PROPERTIES[2] = TASK_ID;
-    }
+	static {
+		PROPERTIES_INTERNAL.add(ID);
+		PROPERTIES_INTERNAL.add(TAG);
+		PROPERTIES_INTERNAL.add(TASK_ID);
+	}
+	static {
+		TABLE.setRowIdProperty(ID);
+	}
 
-    // --- default values
-    protected static final ValuesStorage defaultValues = new Tag().newValuesStorage();
-    static {
-        // --- put property defaults
-    }
+	public Tag() {
+		super();
+	}
 
-    @Override
-    public ValuesStorage getDefaultValues() {
-        return defaultValues;
-    }
+	public Tag(SquidCursor<Tag> cursor) {
+		this();
+		readPropertiesFromCursor(cursor);
+	}
 
-    // --- constants
+	public Tag(ValuesStorage values) {
+		this(values, PROPERTIES);
+	}
 
-    // --- default constructors
-    public Tag() {
-        super();
-    }
+	public Tag(ValuesStorage values, Property<?>... withProperties) {
+		this();
+		readPropertiesFromValuesStorage(values, withProperties);
+	}
 
-    public Tag(SquidCursor<Tag> cursor) {
-        this();
-        readPropertiesFromCursor(cursor);
-    }
+	public Tag(ValuesStorage values, List<Property<?>> withProperties) {
+		this();
+		readPropertiesFromValuesStorage(values, withProperties);
+	}
 
-    public Tag(Map<String, Object> values) {
-        this(values, PROPERTIES);
-    }
+	@Override
+	public ValuesStorage getDefaultValues() {
+		return defaultValues;
+	}
 
-    public Tag(Map<String, Object> values, Property<?>... withProperties) {
-        this();
-        readPropertiesFromMap(values, withProperties);
-    }
+	@Override
+	public Property.LongProperty getRowIdProperty() {
+		return ID;
+	}
 
-    @Override
-    public Tag clone() {
-        return (Tag) super.clone();
-    }
+	/**
+	 * This getter is an alias for getRowId(), as the underlying column is an INTEGER PRIMARY KEY
+	 */
+	public long getId() {
+		return super.getRowId();
+	}
 
-    // --- getters and setters
-    /**
-     * This getter is an alias for getRowId(), as the underlying column is an INTEGER PRIMARY KEY
-     */
-    public long getId() {
-        return super.getRowId();
-    }
+	/**
+	 * This setter is an alias for setRowId(), as the underlying column is an INTEGER PRIMARY KEY
+	 */
+	public Tag setId(long id) {
+		super.setRowId(id);
+		return this;
+	}
 
-    /**
-     * This setter is an alias for setRowId(), as the underlying column is an INTEGER PRIMARY KEY
-     */
-    public Tag setId(long id) {
-        super.setRowId(id);
-        return this;
-    }
+	public String getTag() {
+		return get(TAG);
+	}
 
-    public String getTag() {
-        return get(TAG);
-    }
+	public Tag setTag(String tag) {
+		set(TAG, tag);
+		return this;
+	}
 
-    public Tag setTag(String tag) {
-        set(TAG, tag);
-        return this;
-    }
+	public Long getTaskId() {
+		return get(TASK_ID);
+	}
 
-    public Long getTaskId() {
-        return get(TASK_ID);
-    }
+	public Tag setTaskId(Long taskId) {
+		set(TASK_ID, taskId);
+		return this;
+	}
 
-    public Tag setTaskId(Long taskId) {
-        set(TASK_ID, taskId);
-        return this;
-    }
+	@Override
+	public Tag setRowId(long rowid) {
+		super.setRowId(rowid);
+		return this;
+	}
 
-    @Override
-    public Tag setRowId(long rowid) {
-        super.setRowId(rowid);
-        return this;
-    }
-
+	@Override
+	public Tag clone() {
+		return (Tag) super.clone();
+	}
 }
--- samples/squidb-ios-sample/squidb-ios-sample/squidb-gen/com/yahoo/squidb/sample/models/Task.java
+++ samples/squidb-ios-sample/squidb-ios-sample/squidb-gen/com/yahoo/squidb/sample/models/Task.java
@@ -1,157 +1,156 @@
+// Generated code -- do not modify!
 package com.yahoo.squidb.sample.models;
 
 import com.yahoo.squidb.data.SquidCursor;
 import com.yahoo.squidb.data.TableModel;
 import com.yahoo.squidb.data.ValuesStorage;
 import com.yahoo.squidb.sql.Property;
-import com.yahoo.squidb.sql.Property.IntegerProperty;
-import com.yahoo.squidb.sql.Property.LongProperty;
-import com.yahoo.squidb.sql.Property.StringProperty;
 import com.yahoo.squidb.sql.Table;
 import com.yahoo.squidb.sql.TableModelName;
-import java.util.Map;
+import java.lang.Integer;
+import java.lang.Long;
+import java.lang.Override;
+import java.lang.String;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * This class was generated from the model spec at {@link com.yahoo.squidb.sample.models.TaskSpec}
  */
-// Generated code -- do not modify!
 public class Task extends TableModel {
+	private static final List<Property<?>> PROPERTIES_INTERNAL = new ArrayList<>(5);
 
-    // --- allocate properties array
-    public static final Property<?>[] PROPERTIES = new Property<?>[5];
+	public static final List<Property<?>> PROPERTIES = Collections.unmodifiableList(PROPERTIES_INTERNAL);
 
-    // --- table declaration
-    public static final Table TABLE = new Table(Task.class, PROPERTIES, "tasks", null);
-    public static final TableModelName TABLE_MODEL_NAME = new TableModelName(Task.class, TABLE.getName());
+	public static final Table TABLE = new Table(Task.class, PROPERTIES, "tasks", null, null);
 
-    // --- property declarations
-    public static final LongProperty ID = new LongProperty(TABLE_MODEL_NAME, "_id", "PRIMARY KEY AUTOINCREMENT");
-    static {
-        TABLE.setRowIdProperty(ID);
-    };
+	public static final TableModelName TABLE_MODEL_NAME = new TableModelName(Task.class, TABLE.getName());
 
-    @Override
-    public LongProperty getRowIdProperty() {
-        return ID;
-    }
+	public static final Property.LongProperty ID = new Property.LongProperty(TABLE_MODEL_NAME, "_id", "PRIMARY KEY AUTOINCREMENT");
 
+	public static final Property.StringProperty TITLE = new Property.StringProperty(TABLE_MODEL_NAME, "title", "NOT NULL");
 
-    public static final StringProperty TITLE = new StringProperty(TABLE_MODEL_NAME, "title", "NOT NULL");
+	public static final Property.LongProperty COMPLETION_DATE = new Property.LongProperty(TABLE_MODEL_NAME, "completionDate", "DEFAULT 0");
 
-    public static final LongProperty COMPLETION_DATE = new LongProperty(TABLE_MODEL_NAME, "completionDate", "DEFAULT 0");
+	public static final Property.LongProperty DUE_DATE = new Property.LongProperty(TABLE_MODEL_NAME, "dueDate", "DEFAULT 0");
 
-    public static final LongProperty DUE_DATE = new LongProperty(TABLE_MODEL_NAME, "dueDate", "DEFAULT 0");
+	public static final Property.IntegerProperty PRIORITY = new Property.IntegerProperty(TABLE_MODEL_NAME, "priority", "DEFAULT 0");
 
-    public static final IntegerProperty PRIORITY = new IntegerProperty(TABLE_MODEL_NAME, "priority", "DEFAULT 0");
+	protected static final ValuesStorage defaultValues = new Task().newValuesStorage();
 
-    static {
-        PROPERTIES[0] = ID;
-        PROPERTIES[1] = TITLE;
-        PROPERTIES[2] = COMPLETION_DATE;
-        PROPERTIES[3] = DUE_DATE;
-        PROPERTIES[4] = PRIORITY;
-    }
+	static {
+		PROPERTIES_INTERNAL.add(ID);
+		PROPERTIES_INTERNAL.add(TITLE);
+		PROPERTIES_INTERNAL.add(COMPLETION_DATE);
+		PROPERTIES_INTERNAL.add(DUE_DATE);
+		PROPERTIES_INTERNAL.add(PRIORITY);
+	}
+	static {
+		defaultValues.put(COMPLETION_DATE.getName(), 0L);
+		defaultValues.put(DUE_DATE.getName(), 0L);
+		defaultValues.put(PRIORITY.getName(), 0);
+	}
+	static {
+		TABLE.setRowIdProperty(ID);
+	}
 
-    // --- default values
-    protected static final ValuesStorage defaultValues = new Task().newValuesStorage();
-    static {
-        // --- put property defaults
-        defaultValues.put(COMPLETION_DATE.getName(), 0L);
-        defaultValues.put(DUE_DATE.getName(), 0L);
-        defaultValues.put(PRIORITY.getName(), 0);
-    }
+	public Task() {
+		super();
+	}
 
-    @Override
-    public ValuesStorage getDefaultValues() {
-        return defaultValues;
-    }
+	public Task(SquidCursor<Task> cursor) {
+		this();
+		readPropertiesFromCursor(cursor);
+	}
 
-    // --- constants
+	public Task(ValuesStorage values) {
+		this(values, PROPERTIES);
+	}
 
-    // --- default constructors
-    public Task() {
-        super();
-    }
+	public Task(ValuesStorage values, Property<?>... withProperties) {
+		this();
+		readPropertiesFromValuesStorage(values, withProperties);
+	}
 
-    public Task(SquidCursor<Task> cursor) {
-        this();
-        readPropertiesFromCursor(cursor);
-    }
+	public Task(ValuesStorage values, List<Property<?>> withProperties) {
+		this();
+		readPropertiesFromValuesStorage(values, withProperties);
+	}
 
-    public Task(Map<String, Object> values) {
-        this(values, PROPERTIES);
-    }
+	@Override
+	public ValuesStorage getDefaultValues() {
+		return defaultValues;
+	}
 
-    public Task(Map<String, Object> values, Property<?>... withProperties) {
-        this();
-        readPropertiesFromMap(values, withProperties);
-    }
+	@Override
+	public Property.LongProperty getRowIdProperty() {
+		return ID;
+	}
 
-    @Override
-    public Task clone() {
-        return (Task) super.clone();
-    }
+	/**
+	 * This getter is an alias for getRowId(), as the underlying column is an INTEGER PRIMARY KEY
+	 */
+	public long getId() {
+		return super.getRowId();
+	}
 
-    // --- getters and setters
-    /**
-     * This getter is an alias for getRowId(), as the underlying column is an INTEGER PRIMARY KEY
-     */
-    public long getId() {
-        return super.getRowId();
-    }
+	/**
+	 * This setter is an alias for setRowId(), as the underlying column is an INTEGER PRIMARY KEY
+	 */
+	public Task setId(long id) {
+		super.setRowId(id);
+		return this;
+	}
 
-    /**
-     * This setter is an alias for setRowId(), as the underlying column is an INTEGER PRIMARY KEY
-     */
-    public Task setId(long id) {
-        super.setRowId(id);
-        return this;
-    }
+	public String getTitle() {
+		return get(TITLE);
+	}
 
-    public String getTitle() {
-        return get(TITLE);
-    }
+	public Task setTitle(String title) {
+		set(TITLE, title);
+		return this;
+	}
 
-    public Task setTitle(String title) {
-        set(TITLE, title);
-        return this;
-    }
+	public Long getCompletionDate() {
+		return get(COMPLETION_DATE);
+	}
 
-    public Long getCompletionDate() {
-        return get(COMPLETION_DATE);
-    }
+	public Task setCompletionDate(Long completionDate) {
+		set(COMPLETION_DATE, completionDate);
+		return this;
+	}
 
-    public Task setCompletionDate(Long completionDate) {
-        set(COMPLETION_DATE, completionDate);
-        return this;
-    }
+	public Long getDueDate() {
+		return get(DUE_DATE);
+	}
 
-    public Long getDueDate() {
-        return get(DUE_DATE);
-    }
+	public Task setDueDate(Long dueDate) {
+		set(DUE_DATE, dueDate);
+		return this;
+	}
 
-    public Task setDueDate(Long dueDate) {
-        set(DUE_DATE, dueDate);
-        return this;
-    }
+	public Integer getPriority() {
+		return get(PRIORITY);
+	}
 
-    public Integer getPriority() {
-        return get(PRIORITY);
-    }
+	public Task setPriority(Integer priority) {
+		set(PRIORITY, priority);
+		return this;
+	}
 
-    public Task setPriority(Integer priority) {
-        set(PRIORITY, priority);
-        return this;
-    }
+	@Override
+	public Task setRowId(long rowid) {
+		super.setRowId(rowid);
+		return this;
+	}
 
-    public boolean isCompleted() {
-        return TaskSpec.isCompleted(this);
-    }
+	@Override
+	public Task clone() {
+		return (Task) super.clone();
+	}
 
-    @Override
-    public Task setRowId(long rowid) {
-        super.setRowId(rowid);
-        return this;
-    }
-
+	public boolean isCompleted() {
+		return TaskSpec.isCompleted(this);
+	}
 }
